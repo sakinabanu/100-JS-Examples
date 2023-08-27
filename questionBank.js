76e4ea1b2c85bca7bf3bfdc5fb02eac57a62cf00
sakinabanu <banusakina04@gmail.com>
2023-08-27 16:05:05 +0530

    problem solving

--- questionBank.js
+++ questionBank.js
@@ -10,3 +10,33 @@
 // 8.  Javascript Program to Check if a Number is Odd or Even.
 // 9.  JavaScript Program to Find the Largest Among Three Numbers.
 // 10. JavaScript Program to Check Prime Number.
+// 11. JavaScript program to print all the prime numbers in an Interval.	
+// 12. JavaScript program to find the factorial of the given number.
+// 13. JavaScript program to print the multiplication table.	
+// 14. JavaScript Program to Find the Sum of Natural Numbers.
+// 15. JavaScript Program to Check Whether a String is Palindrome or Not.
+// 16. JavaScript program to generate Fibonacci Series Up to n Terms | Fibonacci Sequence Up to a Certain Number.
+// 17. JavaScript Program to Check Armstrong Number.
+// 18. JavaScript Program to Find Armstrong Number in an Interval.
+// 19. JavaScript Program to Make a Simple Calculator.
+// 20. JavaScript Program to Find HCF | using for Loop, using while Loop and if...else.
+// 21. JavaScript Program to Find LCM.
+// 22. JavaScript Program to Find the Factors of a Number.
+// 23. JavaScript Program to Guess a Random Number.
+// 24. JavaScript Program to Shuffle Deck of Cards.
+// 25. JavaScript Program to Find Sum of Natural Numbers Using Recursion.
+// 26. JavaScript Program to Display Fibonacci Sequence Using Recursion.
+// 27. JavaScript Program to Find Factorial of Number Using Recursion.
+// 28. JavaScript Program to Convert Decimal to Binary.
+// 29. JavaScript Program to Find ASCII Value of Character.
+// 30. JavaScript Program to Sort Words in Alphabetical Order.
+// 31. JavaScript Program to Replace Characters of a String.
+// 32. JavaScript Program to Reverse a String | using function and for loop.
+// 33. Important : JavaScript Program to Create Objects in Different Ways | using object literal, using new keyword, constructor.
+// 34. JavaScript Program to Check the Number of Occurrences of a Character in the String.
+// 35. JavaScript Program to Convert the First Letter of a String into UpperCase.
+// 36. JavaScript Program to Count the Number of Vowels in a String.
+// 37. JavaScript Program to Remove a Property from an Object | also adding a new property.
+// 38. JavaScript Program to Check Whether a String Starts and Ends With Certain Characters.
+// 39. JavaScript Program to Check if a Key Exists in an Object.
+// 40. JavaScript Program to Clone a JS Object.
